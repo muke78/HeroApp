b142a8efbf37f5e7c4c108dbab1bbe9f724ffd36
Erick Muke <muke7881@gmail.com>
2024-08-30 04:38:01 -0600

    Seccion 14: Diseno de la pantalla de busqueda

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -16,10 +16,10 @@ export const HeroCard = ({
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutter">
-          <div className="col-4">
+          <div className="col-4 ">
             <img src={heroImageUrl} className="card-img" alt={superhero} />
           </div>
           <div className="col-8">
--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,18 +1,51 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { Navigate, useParams, useNavigate } from "react-router-dom";
 import { getHeroById } from "../helpers/getHeroById";
 
 export const HeroPage = () => {
   const { id } = useParams();
 
-  const hero = getHeroById(id);
+  const hero = useMemo(() => getHeroById(id), [id]);
 
   if (!hero) {
     return <Navigate to="/" />;
   }
 
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <>
-      <h1>HeroPage</h1>
-    </>
+    <div className="row mt-5 animate__animated animate__fadeIn">
+      <div className="col-4">
+        <img
+          src={`/assets/heroes/${id}.jpg`}
+          alt={hero.superhero}
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+      <div className="col-8">
+        <h3> {hero.superhero} </h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p> {hero.characters} </p>
+        <button o className="btn btn-outline-primary" onClick={onNavigateBack}>
+          Regresar
+        </button>
+      </div>
+    </div>
   );
 };
