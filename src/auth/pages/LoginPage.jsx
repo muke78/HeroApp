97fee7bff78d8b0c35766ea62dccefa14c45817e
Khelde <94265348+muke78@users.noreply.github.com>
2024-09-12 23:53:04 -0600

    feat: Logueo de la aplicacion listo

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "../hooks/useForm";
 
 export const LoginPage = () => {
+  const [error, setError] = useState("");
+
   const navigate = useNavigate();
 
-  const onLogin = () => {
-    navigate("/", {
-      replace: true,
+  const emailLogin = "petite.maryy.05@gmail.com";
+  const passwordLogin = "123456";
+
+  const { formState, username, email, password, onInputChange, onResetForm } =
+    useForm({
+      username: "",
+      email: "",
+      password: "",
     });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (email === emailLogin && password === passwordLogin) {
+      navigate("/", {
+        replace: true,
+      });
+    } else {
+      setError("Credenciales invalidas");
+      onResetForm();
+    }
   };
 
   return (
@@ -16,13 +37,28 @@ export const LoginPage = () => {
       </div>
       <hr />
       <div className="card p-5 mt-5">
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="mb-3">
             <label className="form-label">Username</label>
             <input
               type="text"
               name="username"
+              value={username}
+              onChange={onInputChange}
               className="form-control"
+              placeholder="Tu nombre o usuario"
+              required={true}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Email</label>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={onInputChange}
+              className="form-control"
+              placeholder={emailLogin}
               required={true}
             />
           </div>
@@ -31,14 +67,18 @@ export const LoginPage = () => {
             <input
               type="password"
               name="password"
+              value={password}
+              onChange={onInputChange}
               className="form-control"
+              placeholder={passwordLogin}
               required={true}
             />
           </div>
+          {error && <h4 className="text-danger"> {error} </h4>}
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
         </form>
-        <button className="btn btn-primary" onClick={onLogin}>
-          Login
-        </button>
       </div>
     </div>
   );
