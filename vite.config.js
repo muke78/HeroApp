522b9dc1e8ab58c7357684786158ecd53a5209c3
Erick Muke <muke7881@gmail.com>
2024-09-18 17:38:09 -0600

    chore: Se agrega el renderizado de chunks de rollup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+       manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+          // Aquí puedes crear más chunks para módulos grandes
+          // Si un archivo contiene 'HeavyComponent.js', lo coloca en un chunk separado
+          // if (id.includes("HeavyComponent")) {
+          //   return "heavy-component";
+          // }
+        },
+      },
+    },
+  },
+});
